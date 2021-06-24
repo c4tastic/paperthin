82978778d29f59121b8b1558e8b8bd0e011f07b8
Lucien Bénié <lucien.benie@logmein.com>
2021-06-23 22:38:00 -0400

    chore: add mathieus linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
+/**
+ * @type {import('eslint').ESLint.Options}
+ */
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   project: ['./tsconfig.json'],
-  plugins: [
-    '@typescript-eslint',
-    'prettier'
-  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier'
+    'prettier',
+    require.resolve('@m4thieulavoie/linting'),
   ],
 }
