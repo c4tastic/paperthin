c87d72f8d598dbb5f9efa6086ecb45e175dcc313
Lucien Bénié <lucien.benie@shopify.com>
2021-07-15 16:02:11 -0400

    feat: cleanup the code

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -1,18 +1,41 @@
-import { createElement, RefObject } from 'react'
-import { baseProp, WrapperProps } from './models'
+import { createElement, useEffect } from 'react'
+import type { AllHTMLAttributes, MutableRefObject, ReactNode } from 'react'
 
-const removeClassName = (props?: baseProp) => ({
+interface WrapperProps<T> {
+  tagName: string
+  ref: MutableRefObject<T>
+  props?: AllHTMLAttributes<T>
+  children?: ReactNode
+  events?: ReadonlyMap<string, EventListenerOrEventListenerObject>
+}
+
+const removeClassName = <T>(props?: AllHTMLAttributes<T>) => ({
   ...props,
   class: props?.className,
   className: undefined,
 })
 
-export const createWrapper = <T = baseProp>({
+export const createWrapper = <T extends HTMLElement>({
   tagName,
   ref,
   props,
   children,
+  events,
 }: WrapperProps<T>) => {
-  // TODO: event map handle and extra props
-  return createElement(tagName, removeClassName(props), children)
+  // Maps custom events from a Web Component to a React Component
+  useEffect(() => {
+    const entries = events?.entries() ?? []
+
+    for (const [name, evt] of entries) {
+      ref.current.addEventListener(name, evt)
+    }
+
+    return () => {
+      for (const [name, evt] of entries) {
+        ref.current.removeEventListener(name, evt)
+      }
+    }
+  }, [])
+
+  return createElement(tagName, removeClassName<T>(props), children)
 }
